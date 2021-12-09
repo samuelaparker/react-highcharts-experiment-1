dbe68bc8634f0208021026ee30959f0c25eafc68
samuelaparker <samuel.a.parker00@gmail.com>
2021-12-09 16:01:50 -0500

    add more highchart demos -wip

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import HighchartsReact from 'highcharts-react-official'
 import Highcharts from 'highcharts'
+import { useState } from 'react'
 
-export default class Container extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      chartOptions: {
-        series: [{
-          data: [1,2,3]
-        }]
-      }
-    }
-    setInterval(() => this.setState({
-        chartOptions: {
+export default function Container() {
+
+  const [chartOptions, setChartOptions] = useState({
+    series: [{
+      data: [1,2,3]
+    }]
+  })
+  
+    setInterval(() => setChartOptions({
           series: [{
             data: [
               Math.random()*3,
@@ -21,18 +19,58 @@ export default class Container extends React.Component {
               Math.random()*3
             ]
           }]
-        }}
+        }
       ), 1500)
-  }
+  
+console.log('chartOptions', chartOptions)
 
-  render () {
     return (
       <div>
         <HighchartsReact
           highcharts={Highcharts}
-          options={this.state.chartOptions}
+          options={chartOptions}
         />
       </div> 
     )
-  }
+  
 }
+
+// import React from 'react'
+// import HighchartsReact from 'highcharts-react-official'
+// import Highcharts from 'highcharts'
+// import { useState } from 'react'
+
+// export default class Container extends React.Component {
+//   constructor () {
+//     super()
+//     this.state = {
+//       chartOptions: {
+//         series: [{
+//           data: [1,2,3]
+//         }]
+//       }
+//     }
+//     setInterval(() => this.setState({
+//         chartOptions: {
+//           series: [{
+//             data: [
+//               Math.random()*3,
+//               Math.random()*3,
+//               Math.random()*3
+//             ]
+//           }]
+//         }}
+//       ), 1500)
+//   }
+
+//   render () {
+//     return (
+//       <div>
+//         <HighchartsReact
+//           highcharts={Highcharts}
+//           options={this.state.chartOptions}
+//         />
+//       </div> 
+//     )
+//   }
+// }
